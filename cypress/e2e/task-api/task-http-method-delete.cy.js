d37b4ef7ac4d971031a2e54b9a1af939914257f2
sitikhotimah02 <siti.khotimah.ft17@mail.umy.ac.id>
2023-03-17 11:24:47 +0700

    HTTP Method DELETE

--- cypress/e2e/task-api/task-http-method-delete.cy.js
+++ cypress/e2e/task-api/task-http-method-delete.cy.js
@@ -2,7 +2,7 @@ describe("delete user", () => {
     it("Successfully delete user", () => {
       cy.request("DELETE", "https:reqres.in/api/users/2").then((response) => {
         expect(response.status).equal(204);
-       
+        expect(response.body).to.eq('')
       });
     });
   });
