4327c9e2d1e1c10753605130979624fdb4d9e0a8
sitikhotimah02 <siti.khotimah.ft17@mail.umy.ac.id>
2023-03-20 16:17:02 +0700

    Body Validate

--- cypress/e2e/task-api-2/task-validate-header.cy.js
+++ cypress/e2e/task-api-2/task-validate-header.cy.js
@@ -6,8 +6,16 @@ describe("Validation Body PokeAPI", () => {
       .as("pokemon")
       .then((response) => {
         expect(response.status).equal(200);
-        expect(response.body.name).to.eq("ditto")
-        cy.get("@pokemon").its("headers").its("content-type").should("include", "application/json; charset=utf-8");
-      });
+        expect(response.body.name).to.eq("ditto");
+
+    cy.request("https://pokeapi.co/api/v2/pokemon")
+      .as("pokemon")
+      .then((response) => {
+         expect(response.body.count).to.eq(1281);
+    });
+    
+    cy.get("@pokemon").its("headers").its("content-type").should("include", "application/json; charset=utf-8");
+    
+  });
   });
 });
